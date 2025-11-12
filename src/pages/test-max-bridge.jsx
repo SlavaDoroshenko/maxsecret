6688a3240478999010d0e2873101c56dcf1211be
Вячеслав Дорошенко <44668361+SlavaDoroshenko@users.noreply.github.com>
2025-11-12 17:23:41 +0300

    test max bridge

--- src/pages/test-max-bridge.jsx
+++ src/pages/test-max-bridge.jsx
@@ -3,13 +3,15 @@ import { useEffect, useState } from "react";
 function Test() {
   const [webApp, setWebApp] = useState(null);
   const [userData, setUserData] = useState(null);
+  const [platformData, setPlatformData] = useState(null);
 
   useEffect(() => {
     if (window.WebApp) {
       window.WebApp.ready();
       const app = window.WebApp;
       setWebApp(app);
-      setUserData(app.initDataUnsafe); // ✅ теперь app — это window.WebApp
+      setUserData(app.initDataUnsafe);
+      setPlatformData(app.platform); // ✅ теперь app — это window.WebApp
     }
   }, []);
 
@@ -19,7 +21,8 @@ function Test() {
 
   return (
     <div>
-      <h1>Привет, {userData}!</h1>
+      <h1>Привет, {userData.user.first_name}!</h1>
+      <h1>{platformData}</h1>
       <button onClick={() => webApp.close()}>Закрыть</button>
     </div>
   );
