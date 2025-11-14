b3cc847382f325bda278f74b64ffd8470f900bf5
Вячеслав Дорошенко <44668361+SlavaDoroshenko@users.noreply.github.com>
2025-11-15 02:04:05 +0300

    add new page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import TestRamzan from "./pages/test";
 import Main from "./pages/main";
 import BarcodeScannerPage from "./pages/barcode-scanner";
+import AddNewMeal from "./pages/add-new-meal";
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Main />} />
-    <Route path="/test" element={<TestRamzan />} />
+    <Route
+      path="/test"
+      element={<AddNewMeal meal="Завтрак" date={"Четверг, 29 ноября"} />}
+    />
     <Route path="/barcode" element={<BarcodeScannerPage />} />
   </Routes>
 );
---
+++ src/pages/add-new-meal.jsx
@@ -0,0 +1,48 @@
+import {
+  Panel,
+  Grid,
+  Container,
+  Flex,
+  Avatar,
+  Typography,
+  Button,
+  CellList,
+  CellAction,
+} from "@maxhub/max-ui";
+import TabBar from "../components/tabs";
+import CircularProgress from "../components/circular-progress";
+import { currentNorm, dailyNorm, dailyRemain } from "../mocks/variables";
+import ArrowRight from "../assets/arrow-right.svg?react";
+import NormStatic from "../components/norm-static";
+import MacronutrientBar from "../components/progress-bars";
+import Plus from "../assets/plus.svg?react";
+import BreakFastIcon from "../assets/breakfast.svg?react";
+import MealItem from "../components/meal-item";
+
+const AddNewMeal = ({ meal, date }) => {
+  return (
+    <Panel mode="secondary" className="flex flex-col">
+      <div className="min-h-dvh bg-inherit pb-[70px]">
+        <CellList
+          mode="full-width"
+          filled={true}
+          className=" rounded-b-4xl overflow-hidden "
+        >
+          <Flex direction="column" align="center" className="p-2">
+            <Typography.Headline variant="large-strong" className="mt-2">
+              {meal}
+            </Typography.Headline>
+
+            <Typography.Title variant="custom" className="text-xl">
+              {date}
+            </Typography.Title>
+          </Flex>
+        </CellList>
+
+        <TabBar />
+      </div>
+    </Panel>
+  );
+};
+
+export default AddNewMeal;
