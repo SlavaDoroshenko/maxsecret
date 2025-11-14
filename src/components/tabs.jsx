a872230c6d831113f40cf06a00221c3a2f4f1859
SlavaDoroshenko <lblue1256@gmail.com>
2025-11-14 20:54:48 +0300

    test new components

--- src/components/tabs.jsx
+++ src/components/tabs.jsx
@@ -4,6 +4,7 @@ import ProfileIcon from "../assets/profile.svg?react";
 import ReceptsIcon from "../assets/recepts.svg?react";
 import StatisticIcon from "../assets/statistic.svg?react";
 import { useNavigate } from "react-router-dom";
+import { Panel } from "@maxhub/max-ui";
 
 const tabs = [
   { id: "diary", label: "Дневник", Icon: <DiaryIcon /> },
@@ -14,25 +15,22 @@ const tabs = [
 
 export default function TabBar() {
   const { activeTab, setActiveTab } = useTab();
+  const navigate = useNavigate();
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 z-10 p-4 border-t-1 border-gray-200">
+    <div className="fixed bottom-0 left-0 right-0 z-10 p-4 bg-inherit border-t border-gray-200">
       <div className="flex justify-around items-center h-14 px-2">
         {tabs.map((tab) => {
           const isActive = activeTab === tab.id;
           const textColor = isActive ? "text-blue-600" : "text-gray-500";
-          const navigate = useNavigate();
 
           return (
             <button
               key={tab.id}
               onClick={() => {
                 setActiveTab(tab.id);
-                if (tab.id == "profile") {
-                  navigate("/test");
-                } else if (tab.id == "recepts") {
-                  navigate("/barcode");
-                }
+                if (tab.id === "profile") navigate("/test");
+                else if (tab.id === "recepts") navigate("/barcode");
               }}
               className={`flex flex-col items-center text-xs ${textColor}`}
             >
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -15,58 +15,60 @@ import NormStatic from "../components/norm-static";
 import MacronutrientBar from "../components/progress-bars";
 
 const Main = () => (
-  <Panel mode="primary" className="h-dvw">
-    <Grid gap={12} cols={1}>
-      <Container className="p-4">
-        <Flex direction="column" align="center">
-          <Typography.Headline variant="large-strong" className="mb-5 mt-8">
-            Дневник
-          </Typography.Headline>
+  <Panel mode="secondary" className="h-dvh flex flex-col">
+    <div className="flex-1 overflow-y-auto pb-24">
+      <Grid gap={12} cols={1}>
+        <Container className="p-4">
+          <Flex direction="column" align="center">
+            <Typography.Headline variant="large-strong" className="mb-4 mt-2">
+              Дневник
+            </Typography.Headline>
 
-          <div className="flex flex-row items-center justify-between w-full">
-            <Button mode="link">
-              <ArrowRight className="w-4 h-4 rotate-180" />
-            </Button>
+            <div className="flex flex-row items-center justify-between w-full">
+              <Button mode="link">
+                <ArrowRight className="w-4 h-4 rotate-180" />
+              </Button>
 
-            <Typography.Title>Четверг, 09 ноября</Typography.Title>
+              <Typography.Title>Четверг, 09 ноября</Typography.Title>
 
-            <Button mode="link">
-              <ArrowRight className="w-4 h-4" />
-            </Button>
-          </div>
-          <Flex
-            direction="row"
-            align="center"
-            justify="between"
-            className="w-full justify-between pl-18 pr-18 mt-6"
-          >
-            <NormStatic value={dailyNorm} label="норма" />
-            <CircularProgress
-              value={currentNorm}
-              max={dailyNorm}
-              label="ккал"
-            />
-            <NormStatic value={dailyRemain} label="осталось" />
+              <Button mode="link">
+                <ArrowRight className="w-4 h-4" />
+              </Button>
+            </div>
+            <Flex
+              direction="row"
+              align="center"
+              justify="between"
+              className="w-full justify-between pl-18 pr-18 mt-3"
+            >
+              <NormStatic value={dailyNorm} label="норма" />
+              <CircularProgress
+                value={currentNorm}
+                max={dailyNorm}
+                label="ккал"
+              />
+              <NormStatic value={dailyRemain} label="осталось" />
+            </Flex>
+            <Flex
+              direction="row"
+              align="center"
+              display="inline-flex"
+              justify="between"
+              className="w-full justify-between pt-3"
+            >
+              <MacronutrientBar label="белки" current={12} max={100} unit="г" />
+              <MacronutrientBar label="жиры" current={22} max={100} unit="г" />
+              <MacronutrientBar
+                label="углеводы"
+                current={42}
+                max={100}
+                unit="г"
+              />
+            </Flex>
           </Flex>
-          <Flex
-            direction="row"
-            align="center"
-            display="inline-flex"
-            justify="between"
-            className="w-full justify-between pt-3"
-          >
-            <MacronutrientBar label="белки" current={12} max={100} unit="г" />
-            <MacronutrientBar label="жиры" current={22} max={100} unit="г" />
-            <MacronutrientBar
-              label="углеводы"
-              current={42}
-              max={100}
-              unit="г"
-            />
-          </Flex>
-        </Flex>
-      </Container>
-    </Grid>
+        </Container>
+      </Grid>
+    </div>
 
     <TabBar />
   </Panel>
